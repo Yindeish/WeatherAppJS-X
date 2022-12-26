fc0123a23fa4f278b8e1e3d8b93d86ac69e55168
Yindeish <adeshinaadam03@gmail.com>
2022-12-26 18:38:01 +0100

    third commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,210 +1,140 @@
-// const axios  = require('axios');
+class EventHandler {
 
-class Weather {
+    typing = false;
+
+    inactive = document.querySelector('.inactive');
+    cityWeatherDetails = document.querySelector('.city-weather-details');
+
+    typeInFeid() {
+        this.searchFeild.addEventListener('input', () => {
+            this.toggleDetails();
+        })
+
+        this.searchFeild.addEventListener('keyup', () => {
+            this.typing = true;
+            this.toggleDetails();
+        })
+        this.searchFeild.addEventListener('keydown', () => {
+            this.typing = true;
+            this.toggleDetails();
+        })
+    }
+
+    toggleDetails() {
+        if ( this.typing ) {
+            this.refreshBtn.classList.remove('active');
+            this.searchBtn.classList.add('active');
+        } else {
+            this.refreshBtn.classList.add('active');
+            this.searchBtn.classList.remove('active');
+            this.inactive.classList.remove('show-f');
+            this.cityWeatherDetails.classList.add('show-f');
+        }
+    }
+
+}
+
+class Weather extends EventHandler {
+
     searchFeild = document.querySelector('input');
     searchControl = document.querySelector('.search-control');
     searchBtn = this.searchControl.querySelector('.bi-search');
     refreshBtn = this.searchControl.querySelector('.bi-arrow-clockwise');
+    weatherStatusImage = document.querySelector('.city-weather-details img');
 
-    searchValue = this.searchFeild.value;
-
-    typing = false;
-
-    countriesIsoCodes = null;
-
-    currentCityGeoDetails = null;
+    error = false;
+    errorMessage = null;
 
     search() {
         if( this.searchBtn.classList.contains('active') ) {
-            console.log('searching...');
-
-            // this.useRapidAPI();
-            this.useOpenWeatherAPI();
+            this.getLocation();
         }
         
     }
 
-    useRapidAPI() {
-        this.getAllCountriesIsoCodes();
-    }
+    async fetchData(url) {
 
-    useOpenWeatherAPI() {
-        this.getLocation()
+        return new Promise((resolve, reject) => {
+            const response = fetch(url);
+            if ( response ) {
+                console.log('Good response');
+                resolve(response);
+            } else {
+                console.log('Bad respose');
+                reject(response);
+            }
+        }).then(res => res.json())
     }
 
     async getLocation() {
+
         const searchValue = document.querySelector('input').value.toLowerCase();
 
         const KEY = 'c765905bf9ec88f22af316e9698e709a';
 
-        // https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=c765905bf9ec88f22af316e9698e709a
         const URL  = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${KEY}`;
 
-        this.fetchData('GET', URL)
+        this.fetchData(URL)
         .then(data => {
             console.log(data);
+            const name = data.name;
+            const temp = data.main.temp;
+            const desc = data.weather[0].description;
+            const weatherStatus = data.weather[0].main;
+
+            const weatherData = {
+                name,
+                temp,
+                desc,
+                status: weatherStatus
+            }
+
+            this.renderUI(weatherData);
         })
         .catch(err => {
-            console.log(err);
+            this.error = true;
+            this.errorMessage = err.message;
+            this.renderError(this.errorMessage);
         })
 
     }
 
-    async fetchData(method, url= null, key = null, host = null, other = null) {
-        return new Promise((resolve, reject) => {
-            const xhr  =  new XMLHttpRequest();
-            // xhr.withCredentials = true;
-            xhr.responseType = 'json';
-            
+    renderUI(weatherData) {
+        const celcius  = document.querySelector('.celcius h1');
+        const weatherDescription  = document.querySelector('.weather-description h2');
+        const city  = document.querySelector('.city h2');
 
-            if( other ) {
-                const { url } = other;
-                xhr.open(method, url);
-                xhr.setRequestHeader("accept", "*/*");
-            } else {
-                xhr.open(method, url);
-                xhr.setRequestHeader("X-RapidAPI-Key", key);
-                xhr.setRequestHeader("X-RapidAPI-Host", host);
-            }
-            xhr.onload = () => {
-                if (xhr.status == 200) {
-                    resolve(xhr.response);
-                } else {
-                    reject(xhr.response);
-                }
-            }
-            xhr.onerror = () => {
-                reject(xhr.response);
-            }
+        const { name, temp, desc, status } = weatherData;
 
-            xhr.send();
-        })
+        if( status == 'Clear' ) this.weatherStatusImage.src = './svgs/sunny.svg';
+        if( status == 'Clouds' ) this.weatherStatusImage.src = './svgs/cloudy.svg';
+        if( status == 'Rainy' ) this.weatherStatusImage.src = './svgs/cloudy.svg';
+
+        celcius.textContent = `${(temp - 273).toFixed(2)}`;
+        weatherDescription.textContent = `${desc}`;
+        city.textContent = `${name}`;
+
     }
 
-    async getAllCountriesIsoCodes() {
-        const URL = "https://countries-cities.p.rapidapi.com/location/country/list";
-        const KEY = "7cfe79fb4cmsha97c9c0ea2a5ac4p1fd173jsn0f3c71c9c278";
-        const HOST = 'countries-cities.p.rapidapi.com';
-        
-        this.fetchData('GET', URL, KEY, HOST)
-        .then(data => {
-            this.countriesIsoCodes = Object.keys(data.countries);
-
-            for(let countryIsoCode of this.countriesIsoCodes) {
-                this.getAllCitiesInCountry(countryIsoCode);
-            }
-        })
-        .catch(err => console.error(err))
-    }
-
-    async getAllCitiesInCountry(countryIsoCode) {
-       
-        const URL  = `https://countries-cities.p.rapidapi.com/location/country/${countryIsoCode}/city/list?page=2&per_page=20&population=1501`;
-        const KEY = "7cfe79fb4cmsha97c9c0ea2a5ac4p1fd173jsn0f3c71c9c278";
-        const HOST = 'countries-cities.p.rapidapi.com';
-
-        this.fetchData('GET', URL, KEY, HOST)
-        .then(data => {
-
-            const citiesInCountry = Object.values(data.cities);
-            const citiesDetails = Object.values(citiesInCountry);
-
-            for(let cityDetails of Object.values(citiesDetails)) {
-
-                const gottenCityDetails = this.searchCityInCities(cityDetails);
-                const gottenCityLat = gottenCityDetails.latitude;
-                const gottenCityLong = gottenCityDetails.longitude;
-
-                this.currentCityGeoDetails = {
-                    lat: gottenCityLat,
-                    long: gottenCityLong
-                }
-
-                console.log(this.currentCityGeoDetails, 'currentCityGeoDetails')
-                this.getCityWeatherDetails(this.currentCityGeoDetails);
-            }
-        })
-        .catch(err => console.error(err))
-    }
-
-    searchCityInCities(cityDetails) {
-        const searchValue = document.querySelector('input').value;
-        // console.log( searchValue, 'searchValue', cityDetails.name, 'cityName')
-        if( cityDetails.name.toLowerCase() == searchValue.toLowerCase() ) {
-            return cityDetails;
-        } else return;
-    }
-
-    async getCityWeatherDetails(geoDetails) {
-        console.log('getting weather the weather details...');
-        const { lat, long } = geoDetails;
-        console.log(lat, long);
-
-        const KEY = 'c765905bf9ec88f22af316e9698e709a';
-        const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${KEY}`;
-        const weatherParameters = {
-            url: weatherURL,
-            key: KEY
+    renderError(errorMessage) {
+        if( this.error ) {
+            this.cityWeatherDetails.innerHTML = 
+            /*html*/
+            `
+            <div class="error-block f-centralize-block-xy">
+                <h1>City not found!</h1>
+                <h2>Check spelling</h2>
+                <span>${errorMessage}</span>
+            </div>
+            `;
         }
-
-        this.fetchData('GET', null, null, null, weatherParameters)
-        .then(data => {
-            const weatherInfos = data.weather;
-            console.log(data, weatherInfos);
-        })
-        .catch(err => {
-            console.log(err);
-        })
-
-         // const req  = new XMLHttpRequest();
-        // req.open('GET', weatherURL);
-        // req.withCredentials = true;
-        // req.responseType = 'json';
-
-        // req.onload = () => {
-        //     if( req.status == 200 ) {
-        //         console.log(req.response, 'good res');
-        //     } else {
-        //         console.log(req.response, 'bad res');
-        //     }
-        // }
-        // req.onerror = () => {
-        //     console.log(req.response);
-        // }
-
-        // req.send();
-        // this.renderUI()
-
-    }
-
-    renderUI() {
-
     }
 
     refresh() {
         if( this.refreshBtn.classList.contains('active') ) {
             location.reload();
         }
-        this.searchFeild.addEventListener('keyup', () => {
-            this.typing = true;
-        })
-    }
-}
-
-class EventHandler extends Weather {
-
-    typeInFeid() {
-        this.searchFeild.addEventListener('input', event => {
-            this.typing = true;
-            if ( this.typing ) {
-                this.refreshBtn.classList.remove('active');
-                this.searchBtn.classList.add('active');
-            } else {
-                this.refreshBtn.classList.add('active');
-                this.searchBtn.classList.remove('active');
-            }
-        })
-
+       
     }
 
     run() {
@@ -213,19 +143,21 @@ class EventHandler extends Weather {
             this.refresh();
         })
         this.searchBtn.addEventListener('click', () => {
+            this.typing = false;
             this.search();
+            this.toggleDetails();
         })
+       
     }
-
 }
+
+
 
 class App {
     static init () {
-        const weatherApp = new EventHandler();
+        const weatherApp = new Weather();
         weatherApp.run();
     }
 }
 
 App.init();
-
-// export default App;
